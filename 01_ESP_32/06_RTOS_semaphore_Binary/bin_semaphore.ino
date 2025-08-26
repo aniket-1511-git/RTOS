271fcd69e1518307e653aa068b1e4b2583c2467a
aniket omanna <aniket.omanna15@gmail.com>
2025-08-26 11:21:31 +0530

    Add 01_ESP_32/06_RTOS_semaphore_Binary

---
+++ 01_ESP_32/06_RTOS_semaphore_Binary/bin_semaphore.ino
@@ -0,0 +1,41 @@
+// Binary semaphore Handler
+static SemaphoreHandle_t binSema;
+/*
+* Function Name : 
+* Description   :
+*/
+void readBinSemaphore(void *parameter)
+{
+  uint32_t val = *(int *) parameter;
+  // Release the binary semaphore
+  xSemaphoreGive(binSema);
+  // Print the received value to the serial monitor
+  Serial.print("Data Received : ");
+  Serial.println(val);
+  while(10);
+}
+
+void setup() {
+
+  uint32_t delayVal;
+  Serial.begin(9600);
+  Serial.println("*************** RTOS Semaphore ***************");
+  // Create binary Semaphore
+  binSema =  xSemaphoreCreateBinary();
+  Serial.println("Enter Value for Delay(MS)");
+  // Wait until the user enters a value on Serial Monitor
+  while(Serial.available()<= 0);
+  // Read the integer input from the serial buffer
+  delayVal = Serial.parseInt();
+  Serial.print("Data Sent : ");
+  Serial.println(delayVal);
+  // Function call to run task 1 in core zero
+  xTaskCreatePinnedToCore(readBinSemaphore,"read semaphore",1024,(void*)&delayVal,1,NULL,0);
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  // wait till binary semaphore return
+  xSemaphoreTake(binSema,portMAX_DELAY);
+}
+
+void loop() {
+ 
+}
