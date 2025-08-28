a0f037088fef21fb29a171a559fff46651e6c43e
aniket omanna <aniket.omanna15@gmail.com>
2025-08-28 12:06:52 +0530

    Add 02_LPC_2148_RTOS/05_Mutex

--- 02_LPC_2148_RTOS/05_Mutex/tast.c
+++ 02_LPC_2148_RTOS/05_Mutex/tast.c
@@ -1,34 +1,40 @@
 #include "header.h"
 /*
 * Function Name : void taskA(void *parameter)
-* Description   : Periodically transmits "Task A running" over UART every 1 second.
+* Description   : Periodically transmits "Task A running" over UART 
 */
 
 void taskA(void *parameter)
 {
     while(1)
     {
+			// mutex lock
 			xSemaphoreTake(mutexHandler,portMAX_DELAY);
 			// Send message
 			uart_stringTransmit("Task A Running \r\n");	
+			// mutex release
 			xSemaphoreGive( mutexHandler );
+			// Delay 
 			vTaskDelay(5);
     }
 }
 
 /*
 * Function Name : void taskB(void *parameter)
-* Description   : Periodically transmits "Task B running" over UART every 1 second.
+* Description   : Periodically transmits "Task B running" over UART 
 */
 
 void taskB(void *parameter)
 {
   while(1)
     {
+			// mutex lock
 			xSemaphoreTake(mutexHandler,portMAX_DELAY);
 			// Send message
 			uart_stringTransmit("Task B Running \r\n");
-			xSemaphoreGive( mutexHandler );			
+			// mutex release
+			xSemaphoreGive( mutexHandler );	
+		// Delay 			
 			vTaskDelay(5);
     }
 }
